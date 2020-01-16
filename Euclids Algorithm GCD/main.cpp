1956f0d3e926c5412f90e69f98cbebc06f3a8b5c
Quin'darius <quinwoodz@gmail.com>
2020-01-16 15:59:41 -0500

    Initial Commit

---
+++ Euclids Algorithm GCD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euclids Algorithm GCD
+//
+//  Created by Quin’darius Lyles-Woods on 1/16/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
